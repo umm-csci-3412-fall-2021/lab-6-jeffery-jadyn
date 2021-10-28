4e05b91f75d3945e10f5f548a87db94f131805fc
Jeffrey Miller <mill7707@macross.morris.umn.edu>
2021-10-28 10:50:20 -0500

    Adds functionality for reading and writng threads in client
    Creates threads in client file that read and write to and from the server.
    
    Co-authored-by: JadynSondrol <sondr037@morris.umn.edu>

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -3,6 +3,7 @@ package echoserver;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.ConnectException;
 import java.net.Socket;
 
 public class EchoClient {
@@ -18,6 +19,94 @@ public class EchoClient {
 		InputStream socketInputStream = socket.getInputStream();
 		OutputStream socketOutputStream = socket.getOutputStream();
 
-		// Put your code here.
+		//Starts the reading server thread
+		ReadFromServer reader = new ReadFromServer(socket);
+		Thread firstThread = new Thread(reader);
+		firstThread.start();
+
+		//Starts the writing server thread
+		WriteToServer writer = new WriteToServer(socket);
+		Thread secondThread = new Thread(writer);
+		secondThread.start();
+		
+		
+	}
+
+	public class ReadFromServer implements Runnable{
+
+		//Declares fields
+		private Socket socket;
+
+		private int nextByte;
+
+		InputStream input;
+
+		//Constructs the class
+		public ReadFromServer(Socket socket) throws IOException {
+			this.input = socket.getInputStream();
+			this.socket = socket;
+		}
+
+		@Override
+		public void run() {
+			try {
+				while ((nextByte = input.read()) != -1) {
+					//reads it from the server and writes it out
+					System.out.write(nextByte);
+					System.out.flush();
+				  }
+				  socket.close();
+			} catch (ConnectException ce) {
+				System.out.println("We were unable to connect to localhost");
+				System.out.println("You should make sure the server is running.");
+			  } catch (IOException ioe) {
+				System.out.println("We caught an unexpected exception");
+				System.err.println(ioe);
+			  }
+		
+		}
+
+
+
+	}
+
+	public class WriteToServer implements Runnable{
+
+		//Declares fields
+		private Socket socket;
+
+		private int nextByte;
+
+		OutputStream output;
+
+		//Contructs class
+		public WriteToServer(Socket socket) throws IOException {
+			this.output = socket.getOutputStream();
+			this.socket = socket;
+		}
+
+		@Override
+		public void run() {
+
+			try {
+				while ((nextByte = System.in.read()) != -1) {
+					//Sends data to the server
+					output.write(nextByte);
+					output.flush();
+				  }
+
+				  socket.shutdownOutput();
+			} catch (IOException ioe) {
+				System.out.println("We caught an unexpected exception");
+				System.err.println(ioe);
+			  }
+
+		
+
+			
+		}
+
+
+
 	}
 }
